e7f57d59fdaaae13bcc046c9438c57fe11f9e05b
jereBrand <reafdaw@gmail.com>
2018-04-23 19:07:05 +0200

    first version of R summary script

---
+++ scripts/summarize_bt.R
@@ -0,0 +1,85 @@
+library(ggplot2)  
+library(tidyr)
+library(dplyr)
+
+################################################################################
+
+# Summarize the number of shifts
+params <- 100
+col_names <- c("file", "tree", "No_tips", "clade_proportion", "birth",
+               "death", "rate_delta", "tool", "prior_set" ,"chain", "It", "Lh", "Lh_and_Prior", "No_Pram", "Alpha", "Sigma_square", "Scale_Prior",
+               paste(rep(c("Node_ID", "Scaler", "Create_It", "Node_Branch"),params),
+                     rep(1:params, each = 4)), sep = "_")
+
+# combine all the files into one dataframe
+All_VarRes <- do.call(rbind,
+                           lapply(paste0("out/bt/", rates_f),
+                                  read.table, fill = TRUE, sep = "\t", col.names = col_names))
+All_VarRes$No_tips <- factor(All_VarRes$No_tips,
+                   levels = c("N10", "N50", "N100", "N200", "N400"))
+
+var_sum <-  unite(All_VarRes, run, tree:tool, sep="_")
+
+p  <- ggplot(data = All_VarRes, aes(No_Pram, x = No_tips, color = factor(chain)))
+p  <- p + geom_boxplot(alpha=0.55) + facet_wrap(~ rate_delta) + theme_bw()
+p
+
+
+p  <- ggplot(data = var_sum, aes(x = factor(No_Pram), y = Lh, color=No_Pram))
+p  <- p + geom_point(alpha=0.55) + facet_wrap(~ run, scales = "free") + theme_bw()
+p
+
+
+
+lh_cols <- c("file", "tree", "No_tips", "clade_proportion", "birth",
+             "death", "rate_delta", "tool", "prior_set" ,"chain", "Lh")
+marginal_lh <- read.table("out/bt/bt.marginalLH.tsv", col.names =  lh_cols)
+marginal_lh$No_tips <- factor(marginal_lh$No_tips,
+                              levels = c("N10", "N50", "N100", "N200", "N400"))
+
+p <- ggplot(marginal_lh)
+p + geom_boxplot(aes(x=No_tips, y=Lh, color = prior_set), width = 0.25) +
+  ylab("Marginal likelihood") + xlab("Tree size")+
+  facet_wrap(~ rate_delta) + theme_bw()
+
+################################################################################
+
+# Bayes Factor
+
+################################################################################
+# Summary table that combines marginal likelihood
+marginal_lh_combined <- marginal_lh %>%
+  unite(run, tree:tool, sep="_") %>%
+  group_by(run, prior_set) %>%
+    summarise(Lh = mean(Lh))
+
+# calculate the BF for each contrast
+df = marginal_lh_combined
+BF_table <- data.frame(run = rep(NA,length(unique(df$run))),
+                       BF = rep(NA,length(unique(df$run))))
+i = 1
+for (r in unique(df$run)){
+  simpleMod = filter(df, run == r, prior_set == "prNull1")$Lh
+  complexMod = filter(df, run == r, prior_set == "pr1")$Lh
+  BF = 2*(complexMod - simpleMod)
+  BF_table[i, 1] = r
+  BF_table[i, 2] = BF
+  i = i + 1
+}
+
+# output the BF table
+write.table(x = BF_table, file = "out/bt/BF_bt_chainsCombined.tsv", row.names = F)
+
+BF_table %>%
+  separate(run, sep = "_", c("tree", "No_tips", "clade_proportion", "birth",
+           "death", "rate_delta", "tool")) %>%
+  mutate(No_tips = factor(No_tips, levels = c("N10", "N50", "N100", "N200", "N400"))) %>%
+  ggplot() +
+  geom_jitter(aes(x=No_tips, y=BF), width=0.1) +
+  ylab("Bayes Factor") + xlab("Tree size")+
+  facet_wrap(~ rate_delta, scales = "free") + 
+  geom_hline(yintercept=10, linetype="dashed", 
+             color = "red", size=2) +
+  geom_hline(yintercept=0, linetype="dashed", 
+             color = "blue", size=2)+ theme_bw()
+
